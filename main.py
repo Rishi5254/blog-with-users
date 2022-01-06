055489a82266566a5c0ebfec758a35f41a581652
rishi <rishirishik18@gmail.com>
2022-01-06 14:07:47 +0530

    complete blog with users

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class BlogPost(db.Model):
 class User(UserMixin, db.Model):
 
     __tablename__ = 'Users'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)

e2e0475eecc2d59a5792b984815514e9c1a4d7c9
rishi <rishirishik18@gmail.com>
2022-01-21 22:10:21 +0530

    multi users allow

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ from flask_ckeditor import CKEditor
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm
-from flask_gravatar import Gravatar
-from functools import wraps
-from flask import abort
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -45,7 +42,6 @@ class BlogPost(db.Model):
 
 
 class User(UserMixin, db.Model):
-
     __tablename__ = 'Users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -57,35 +53,10 @@ class User(UserMixin, db.Model):
 db.create_all()
 
 
-ADMIN_EMAIL = 'jxaidjisisjs@byom.de'
-
-
-def admin_check():
-    try:
-        current_email = current_user.email
-    except:
-        current_email = "hi"
-    if current_email == ADMIN_EMAIL:
-        return False
-    else:
-        return True
-
-
-def admin_only(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        # If id is not 1 then return abort with 403 error
-        if current_user.email != ADMIN_EMAIL:
-            return abort(403)
-        # Otherwise continue with the route function
-        return f(*args, **kwargs)
-    return decorated_function
-
-
 @app.route('/')
 def get_all_posts():
     posts = BlogPost.query.all()
-    return render_template("index.html", all_posts=posts, logged_in=current_user.is_authenticated, admin=admin_check())
+    return render_template("index.html", all_posts=posts, logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -136,21 +107,24 @@ def logout():
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
     requested_post = BlogPost.query.get(post_id)
-    return render_template("post.html", post=requested_post, admin=admin_check())
+    if current_user.name == requested_post.author:
+        user = True
+    else:
+        user = False
+    return render_template("post.html", post=requested_post, logged_in=current_user.is_authenticated, user=user)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", logged_in=current_user.is_authenticated)
 
 
-@app.route("/new-post")
-@admin_only
+@app.route("/new-post", methods=['GET', 'POST'])
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -159,44 +133,40 @@ def add_new_post():
             subtitle=form.subtitle.data,
             body=form.body.data,
             img_url=form.img_url.data,
-            author=current_user,
+            author=current_user.name,
             date=date.today().strftime("%B %d, %Y")
         )
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form)
+    return render_template("make-post.html", form=form, logged_in=current_user.is_authenticated)
 
 
-@app.route("/edit-post/<int:post_id>")
-@admin_only
+@app.route("/edit-post/<int:post_id>", methods=['GET', 'POST'])
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
     edit_form = CreatePostForm(
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
         body=post.body
     )
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
-
-    return render_template("make-post.html", form=edit_form)
+    return render_template("make-post.html", form=edit_form, logged_in=current_user.is_authenticated, is_edit=True)
 
 
 @app.route("/delete/<int:post_id>")
-@admin_only
 def delete_post(post_id):
     post_to_delete = BlogPost.query.get(post_id)
     db.session.delete(post_to_delete)
     db.session.commit()
+
     return redirect(url_for('get_all_posts'))
 
 
